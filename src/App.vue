756b0343618e7efb201dce67ffa00d6d9ad516c2
pablomgProgOO <mgpablo.co@gmail.com>
2026-02-17 16:13:12 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { ref, watch } from 'vue'
+import ChildComponent from './components/ChildComponent.vue'
+
+const textValue = ref('')
+const selectedValue = ref('option1')
+watch(textValue, (newValue, oldValue) => {
+    console.log(`Le texte est passé de ${oldValue} à ${newValue}`)
+})
+
+watch(selectedValue, (newValue, oldValue) => {
+    console.log(`L'option sélectionnée est passée de ${oldValue} à ${newValue}`)
+})
+</script>
+
+<template>
+  
+  <HelloWorld msg="Vite + Vue" />
+  <div class="container">
+    <h2>Communication Parent-Enfant avec v-model</h2>
+    <p>Texte dans le parent: {{ textValue }}</p>
+    <p>Option sélectionnée dans le parent: {{ selectedValue }}</p>
+    <ChildComponent v-model:textValue="textValue" v-model:selectedValue="selectedValue" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { watch } from 'vue'
+
+const props = defineProps<{ textValue: string; selectedValue: string }>()
+const emit = defineEmits<{
+  (e: 'update:textValue', v: string): void
+  (e: 'update:selectedValue', v: string): void
+}>()
+watch(() => props.textValue, (newValue) => {
+    console.log('Nouvelle valeur du champ texte :', newValue)
+})
+
+watch(() => props.selectedValue, (newValue) => {
+    console.log('Nouvelle valeur sélectionnée :', newValue)
+})
+</script>
+<template>
+ <div>
+    <input 
+      type="text" 
+      :value="textValue" 
+      @input="emit('update:textValue', ($event.target as HTMLInputElement).value)"
+    />
+
+    <select :value="selectedValue" @change="emit('update:selectedValue', ($event.target as HTMLSelectElement).value)">
+      <option value="option1">Option 1</option>
+      <option value="option2">Option 2</option>
+      <option value="option3">Option 3</option>
+    </select>
+  </div>
+</template>
+<style scoped>
+
+</style>
